ad4ea11fa2af5d16dfb4cced0c102005571e0908
ravina116 <makwanaravina116@gmail.com>
2025-05-17 12:45:20 +0000

    virsion1

--- exc1.java
+++ exc1.java
@@ -1,9 +1,9 @@
 public class EXc1
 { 
-    public Static void main(String args[]){
+    public static void main(String args[]){
          int height =10;
          int width =15;
          double area = height * width;
-         System.out println("area=" + area);
+         System.out.println("Area=" + area);
     }    
 }
