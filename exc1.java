712164d19f32d2d0b93850fe9377c75ba0b9361a
ravina116 <makwanaravina116@gmail.com>
2025-05-17 12:35:35 +0000

    version1

---
+++ exc1.java
@@ -0,0 +1,9 @@
+public class EXc1
+{ 
+    public Static void main(String args[]){
+         int height =10;
+         int width =15;
+         double area = height * width;
+         System.out println("area=" + area);
+    }    
+}
